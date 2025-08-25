0c5de6ef6ef96b0713daf7d579f4198b79ee23ea
RamenStudios <adasanrodriguez@gmail.com>
2025-08-25 17:38:43 -0400

    fresh vite install

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import ramenLogo from '/icon.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">
+          <img src={ramenLogo} className="logo" alt="Ramen logo" />
+        </a>
+      </div>
+      <h1>Under construction</h1>
+    </>
+  )
+}
+
+export default App
--- src/components/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App()
-{
-    return(
-        <div>
-            Website currently under construction!
-        </div>
-    )
-}
