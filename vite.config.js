92675c8fbba642b07449d7548a01da9b1902fa5b
RamenStudios <adasanrodriguez@gmail.com>
2025-08-25 17:28:16 -0400

    base default

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const REPO_NAME = '/RamenStudios.github.io/'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `${REPO_NAME}`,
 })
