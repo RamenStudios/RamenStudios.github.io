ac0c1dfeb4f82d120bb6fec6e0e6bd1c1d4e2645
Adamari R <adasanrodriguez@gmail.com>
2026-07-22 13:40:16 -0400

    trying to fix sizing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 import ramenLogo from '/icon.svg'
 
 function App() {
   return (
     <div>
+      <Header logo={ramenLogo}/>
       <div class="container-lg">
         <a href="/" target="_blank">
           <img src={ramenLogo} className="logo spin" alt="Ramen logo" />
