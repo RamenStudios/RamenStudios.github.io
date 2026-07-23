33e836dc8fdb70c90c212f15546256db6c02b5b7
Adamari R <adasanrodriguez@gmail.com>
2026-07-23 10:58:21 -0400

    fixing navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,27 @@
 
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const Header = ({logo, current}) =>
 {
     return(
-        <nav class="navbar sticky-top navbar-expand-lg mb-3 bebas  overflow-hidden" style={{backgroundColor: '#000000ff', height: '4em'}} data-bs-theme="dark">
-            <div class="container-fluid">
-                    <a href="/"><img src={logo} className='navlogo' alt="Ramenstudios Logo"  width="310" height="304"/></a>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active mt-2 mx-2" aria-current="page" href="#"><h1>Home</h1></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mt-2 mx-2" href="#"><h1>About</h1></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mt-2 mx-2" href="https://github.com/RamenStudios"><h1>Github</h1></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mt-2 mx-2" href="#"><h1>Projects</h1></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mt-2 mx-2" href="#"><h1>Portfolio</h1></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link mt-2 mx-2" href="#"><h1>Videos</h1></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <Navbar expand="lg" class="bebas" style={{backgroundColor: '#000000ff', height: '4em'}} data-bs-theme="dark">
+            <Container fluid>
+                <Navbar.Brand className="navlogopos" href="/"><img src={logo} className="navlogo" alt="Ramenstudios Logo"  width="310" height="304"/></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarNav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Home</h1></Nav.Link>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>About</h1></Nav.Link>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="https://github.com/RamenStudios"><h1>Github</h1></Nav.Link>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Projects</h1></Nav.Link>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Portfolio</h1></Nav.Link>
+                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Videos</h1></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
