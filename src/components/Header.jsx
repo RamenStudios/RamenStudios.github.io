f077526219605d06be9c2694d44a3746d0c87543
Adamari R <adasanrodriguez@gmail.com>
2026-07-23 14:08:09 -0400

    responsive navbar yippee

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,18 +9,24 @@ export const Header = ({logo, current}) =>
     return(
         <Navbar expand="lg" class="bebas" style={{backgroundColor: '#000000ff', height: '4em'}} data-bs-theme="dark">
             <Container fluid>
-                <Navbar.Brand className="navlogopos" href="/"><img src={logo} className="navlogo" alt="Ramenstudios Logo"/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarNav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Home</h1></Nav.Link>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>About</h1></Nav.Link>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="https://github.com/RamenStudios"><h1>Github</h1></Nav.Link>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Projects</h1></Nav.Link>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Portfolio</h1></Nav.Link>
-                        <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Videos</h1></Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                    <div className="d-inline-block col-md-3">
+                        <Navbar.Brand  href="/">
+                            <img src={logo} className="navlogo d-none d-lg-inline-block" alt="Ramenstudios Logo"/>
+                        </Navbar.Brand>
+                    </div>
+                    <div className="d-inline-block col-xxl-9">
+                        <Navbar.Toggle aria-controls="navbarNav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Home</h1></Nav.Link>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>About</h1></Nav.Link>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="https://github.com/RamenStudios"><h1>Github</h1></Nav.Link>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Projects</h1></Nav.Link>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Portfolio</h1></Nav.Link>
+                                <Nav.Link class="active mt-2 mx-2" aria-current="page" href="#"><h1>Videos</h1></Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
             </Container>
         </Navbar>
     )
