c3f95bd4f480ee542058ac8b3a8c7d14273a1aeb
Adamari R <adasanrodriguez@gmail.com>
2026-07-29 12:34:55 -0400

    homepage layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,8 +6,8 @@ export const Home = () => {
     return (
         <div class="container-lg justify-content-center">
             <div 
-            class="row mb-3 align-items-center justify-content-center bg-danger border border-danger border-5"
-            style={{minHeight:"20em"}}
+                class="row mb-3 align-items-center justify-content-center bg-danger border border-danger border-5"
+                style={{minHeight:"20em"}}
             >
                 <div class="col-lg-3 col-sm-6 border border-danger border-5">
                     <ImgCard 
@@ -40,31 +40,31 @@ export const Home = () => {
             </div>
             
             <div 
-            class="row mb-3 align-items-center justify-content-center bg-danger border border-danger border-5"
-            style={{height:"20em"}}
+                class="row mb-3 align-items-center justify-content-center bg-danger border border-danger border-5"
+                style={{minHeight:"20em"}}
             >
-                <div class="col-lg-3 col-sm-12 border border-danger border-5">
+                <div class="col-lg-3 col-sm-6 border border-danger border-5">
                     <ImgCard 
                         source={"https://media.tenor.com/GzegOh-0eR4AAAAe/sparkle-on-jerma.png"} 
                         alt={"jerma placeholder"} 
                         caption={"COMICS"}
                     />
                 </div>
-                <div class="col-lg-3 col-sm-12 border border-danger border-5">
+                <div class="col-lg-3 col-sm-6 border border-danger border-5">
                     <ImgCard 
                         source={"https://media.tenor.com/GzegOh-0eR4AAAAe/sparkle-on-jerma.png"} 
                         alt={"jerma placeholder"} 
                         caption={"ILLUSTRATION"}
                     />
                 </div>
-                <div class="col-lg-3 col-sm-12 border border-danger border-5">
+                <div class="col-lg-3 col-sm-6 border border-danger border-5">
                     <ImgCard 
                         source={"https://media.tenor.com/GzegOh-0eR4AAAAe/sparkle-on-jerma.png"} 
                         alt={"jerma placeholder"} 
                         caption={"ANIMATION"}
                     />
                 </div>
-                <div class="col-lg-3 col-sm-12 border border-danger border-5">
+                <div class="col-lg-3 col-sm-6 border border-danger border-5">
                     <ImgCard 
                         source={"https://media.tenor.com/GzegOh-0eR4AAAAe/sparkle-on-jerma.png"} 
                         alt={"jerma placeholder"} 
